f431a8812a7e6424d65b839ede0b7704d1bb619a
juicyenc <huang_yue_zhi@outlook.com>
2020-07-17 22:25:10 +0800

    Update to BigInt and other changes.

--- lib.js
+++ lib.js
@@ -6,7 +6,7 @@ function processString(str) {
     if (!(str instanceof String)) {
         throw new Error("file-size-tag/lib.js: none string parmeter is delivered to processString()");
     } else {
-        var regex = /^(\d+)({KMGTEBGYkmgtebgy})(i?)({Bb})$/.compile();
+        var regex = /^(\d+)({KMGTEBZYkmgtebgy})(i?)({Bb})$/.compile();
         var result = str.match(regex);
         if (result === null) {
             throw new Error(`${str} is not data size.`);
@@ -14,26 +14,33 @@ function processString(str) {
             var is1024 = result[3] === 'i';
             var isbit = result[4] === 'b';
             var prefix = result[2];
-            var base = Number.parseInt(result[1]);
-            var multiple;
+            var base = BigInt(Number.parseInt(result[1]));
+            var multiple = new BigInt();
             switch (prefix.toUpperCase()) {
                 case 'K':
-                    multiple = (is1024 ? 1 << 10 : 1000);
+                    multiple = (is1024 ? BigInt(1) << 10 : BigInt(1000));
                     break;
                 case 'M':
-                    multiple = (is1024 ? 1 << 20 : 1000000);
+                    multiple = (is1024 ? BigInt(1) << 20 : BigInt(1000000));
                     break;
                 case 'G':
-                    multiple = (is1024 ? 1 << 30 : 1000000000);
+                    multiple = (is1024 ? BigInt(1) << 30 : BigInt(1000000000));
                     break;
                 case 'T':
-                    multiple = (is1024 ? 1 << 40 : 1000000000000);
+                    multiple = (is1024 ? BigUInt(1) << 40 : BigInt(1000000000000));
                     break;
                 case 'E':
-                    multiple = (is1024 ? 1 << 50 : 1000000000000000);
+                    multiple = (is1024 ? BigInt(1) << 50 : BigInt(1000000000000000));
                     break;
-                default:
-                    throw new Error(`${str} is too large to calculate.`);
+                case 'B':
+                    multiple = (is1024 ? BigInt(1) << 60 : BigInt(1000000000000000000));
+                    break;
+                case 'Z':
+                    multiple = (is1024 ? BigInt(1) << 70 : BigInt(1000000000000000000000));
+                    break;
+                case 'Z':
+                    multiple = (is1024 ? BigInt(1) << 80 : BigInt(1000000000000000000000000));
+                    break;
             }
             var answer = multiple * base;
             return isbit ? answer : answer << 3;
